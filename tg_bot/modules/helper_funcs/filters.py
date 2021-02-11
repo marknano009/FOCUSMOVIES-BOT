49624b0163e5abbf35b26645c5ad8a3100bbce3e
marknano009 <78907926+marknano009@users.noreply.github.com>
2021-02-11 17:51:01 +0530

    Update filters.py

--- tg_bot/modules/helper_funcs/filters.py
+++ tg_bot/modules/helper_funcs/filters.py
@@ -6,13 +6,13 @@ from tg_bot import SUPPORT_USERS, SUDO_USERS
 
 class CustomFilters(object):
     class _Supporters(BaseFilter):
-        def filter(self, message: Message):
+        def add(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
 
     support_filter = _Supporters()
 
     class _Sudoers(BaseFilter):
-        def filter(self, message: Message):
+        def add(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUDO_USERS)
 
     sudo_filter = _Sudoers()
@@ -22,7 +22,7 @@ class CustomFilters(object):
             self.mime_type = mimetype
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
 
-        def filter(self, message: Message):
+        def add(self, message: Message):
             return bool(message.document and message.document.mime_type == self.mime_type)
 
     mime_type = _MimeType
